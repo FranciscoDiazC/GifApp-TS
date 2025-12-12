cb6bec74be2f96b2974c49c2077df193a08e3cac
FranciscoDC <francisco.diaz1901@alumnos.ubiobio.cl>
2025-12-12 19:50:56 -0300

    refactor: cambiar cache de gifs de useState a useRef

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
+
+
 
 export const useGifs = () => {
 
     const [searchList, setSearchList] = useState<string[]>([]);
     const [gifList, setGifList] = useState<Gif[]>([]);
 
-    const [gifsCache, setGifsCache] = useState<Record<string, Gif[]>>({});
+    const gifsCache = useRef<Record<string, Gif[]>>({});
 
 
     const handleSearch = async (searchTerm: string) => {
@@ -21,42 +23,31 @@ export const useGifs = () => {
                 setSearchList((list) => [...list.slice(1), searchTerm]);
             }
 
-            if(searchTerm in gifsCache){
+            if(searchTerm in gifsCache.current){
                 console.log("El termino buscado esta en cache, evitando realizar solicitud HTTP.");
-                setGifList(gifsCache[searchTerm]);
+                setGifList(gifsCache.current[searchTerm]);
             }else{
                 console.log("El termino buscado no esta cacheado, realizando solicitud HTTP.");
                 // Fetcheamos
                 const response = await getGifsByQuery(import.meta.env.VITE_GIF_API_KEY, searchTerm, 10, "es");
                 console.log(response)
                 setGifList(response);
-                setGifsCache((cache) => ({
-                    ...cache,
-                    [searchTerm]: response // [] computed property name, se usa cuando NO se conoce el nombre de la key, sin los corchetes se guardaria la propiedad searchTerm, y no su valor.
-                }));
+                gifsCache.current[searchTerm] = response; // Agregamos la busqueda a cache
             }
         }
     }
   
     const onPreviousSearchesClicked = async (searchTerm: string) => {
-        if(searchTerm in gifsCache){
+        if(searchTerm in gifsCache.current){
             console.log("El termino buscado esta en cache, evitando realizar solicitud HTTP.");
-            setGifList(gifsCache[searchTerm]);
+            setGifList(gifsCache.current[searchTerm]);
         }else{
             console.log("El termino buscado no esta cacheado, realizando solicitud HTTP.");
-
-            console.log(gifsCache);
-            console.log(searchTerm);
-
-
             // Fetcheamos
             const response = await getGifsByQuery(import.meta.env.VITE_GIF_API_KEY, searchTerm, 10, "es");
             console.log(response)
             setGifList(response);
-            setGifsCache((cache) => ({
-                ...cache,
-                [searchTerm]: response // [] computed property name, se usa cuando NO se conoce el nombre de la key, sin los corchetes se guardaria la propiedad searchTerm, y no su valor.
-            }));
+            gifsCache.current[searchTerm] = response; // Agregamos la busqueda a cache
         }
     }
 
